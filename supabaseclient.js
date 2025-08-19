824b7cae0d100c59d4dfea5611bf55b48fba1e6c
TTS-Projects-lab <ttsprojects21@gmail.com>
2025-08-19 23:06:13 +0530

    Update supabaseclient.js

--- supabaseclient.js
+++ supabaseclient.js
@@ -1,7 +1,6 @@
 // lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ieqlswwdfobuuahxyowh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllcWxzd3dkZm9idXVhaHh5b3doIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1OTU2MTQsImV4cCI6MjA2NjE3MTYxNH0.kVfRidaDIH-uABmkbWf7yr0YlZmRkbtOuGFnN2KePFI
-';
+const supabaseUrl = 'https://djidtlgtpksggigizums.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqaWR0bGd0cGtzZ2dpZ2l6dW1zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyMDE0MjUsImV4cCI6MjA2Nzc3NzQyNX0.RoItW0ILUua0VXaWLzkWWvHQ8szdsBD7oaybSXKkTqQ';
 export const supabase = createClient(supabaseUrl, supabaseKey);
